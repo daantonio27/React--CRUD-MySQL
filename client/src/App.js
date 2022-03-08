0abd09db63551ec49706bed65aae91dca92ab848
daantonio <daantonioc3@gmail.com>
2022-03-08 17:36:07 +0000

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="register-container">
+        <h1 className="register-title">Scrim Shop</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register-input"
+        />
+        <input
+          type="text"
+          placeholder="Preço"
+          name="cost"
+          className="register-input"
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+          name="category"
+          className="register-input"
+        />
+
+        <button className="register-button">Cadastrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
